01e214b2707b588327db6f7450e1a9a45b061be9
Sean Arca <Sean@dhcp-10-105-92-82.wireless.northwestern.private>
2018-01-16 14:26:58 -0600

    add on click submit to push to new child

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,3 +8,28 @@ var config = {
     messagingSenderId: "803440655254"
   };
 firebase.initializeApp(config);
+
+// Create a variable to reference the database.
+var database = firebase.database();
+
+// Initial Values
+var train = "";
+var destination = "";
+var firstTrain = "";
+var frequency = 0;
+
+// On click submit form
+$(".submitBtn").on('click', function(){
+    train = $(".trainName").val().trim();
+    destination = $(".destination").val().trim();
+    firstTrain = $(".firstTrain").val().trim();
+    frequency = $(".frequency").val().trim();
+  
+    // Push data from form to Firebase
+    database.ref().push({
+        trainName: train,
+        destination: destination,
+        firstTrain: firstTrain,
+        frequency: frequency
+    })
+});
