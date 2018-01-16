2779cab22985df0c5ada90b2b02d21e462159399
Sean Arca <Sean@dhcp-10-105-92-82.wireless.northwestern.private>
2018-01-16 13:43:25 -0600

    initialize firebase

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyB1KZxoepwJ9dZu8moNhJmgDqlOHKWjWAw",
+    authDomain: "choo-choo-4343d.firebaseapp.com",
+    databaseURL: "https://choo-choo-4343d.firebaseio.com",
+    projectId: "choo-choo-4343d",
+    storageBucket: "choo-choo-4343d.appspot.com",
+    messagingSenderId: "803440655254"
+  };
+firebase.initializeApp(config);
